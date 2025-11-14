b2097d15aa4fe5338fa28aea4554b248d8507f6d
Dario Ureña <darioaxel@gmail.com>
2025-11-14 13:51:02 +0100

    copiados directorios y ficheros

--- nuxt4-boilerplate/nuxt.config.ts
+++ nuxt4-boilerplate/nuxt.config.ts
@@ -1,8 +1,68 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  components: [
+    {
+      path: '~/components',
+      extensions: ['.vue'],
+    },
+  ],
+
   modules: [
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxt/fonts',
     'nuxt-auth-utils'
   ],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "~/components/ui"
+     */
+    componentDir: '~/components/ui',
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+  fonts: {
+    defaults: {
+      weights: [300, 400, 500, 600, 700, 800],
+    },
+  },
+
+  routeRules: {
+    '/components': { redirect: '/components/accordion' },
+    '/settings': { redirect: '/settings/profile' },
+  },
+
+  imports: {
+    dirs: [
+      './lib',
+    ],
+  }, 
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },  
 })
